1077561d044a3834a7142ad2beed736cfa03ec33
inghamn <inghamn@bloomington.in.gov>
2015-08-05 10:30:11 -0400

    Working, empty website, running on Windows IIS

--- build.sh
+++ build.sh
@@ -12,6 +12,10 @@ if [ ! -d $DIST ]
 	then mkdir $DIST
 fi
 
+cd $DIR/public/css
+./build_css
+cd $DIR
+
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
 rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
