fe27282cb13fc184c27361200fc9708d116aa0ef
inghamn <inghamn@bloomington.in.gov>
2015-08-05 10:46:27 -0400

    Created ODBC database class

---
+++ Models/Database.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Singleton for the Database connection
+ *
+ * @copyright 2015 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+namespace Application\Models;
+
+class Database
+{
+	private static $connection;
+
+	/**
+	 * @param boolean $reconnect If true, drops the connection and reconnects
+	 * @return resource
+	 */
+	public static function getConnection($reconnect=false)
+	{
+		if ($reconnect) {
+			self::$connection=null;
+		}
+		if (!self::$connection) {
+			try {
+                self::$connection = new PDO(DB_DSN, DB_USER, DB_PASS);
+			}
+			catch (Exception $e) {
+				die($e->getMessage());
+			}
+		}
+		return self::$connection;
+	}
+}
